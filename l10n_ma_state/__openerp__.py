162775c40cc962c17534cd0b5ab7358bf2824152
mouadbanane <mouadbanane@gmail.com>
2016-01-09 12:18:15 +0000

    autre changement

--- l10n_ma_state/__openerp__.py
+++ l10n_ma_state/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Morocco States (Régions)',
-    'summary': 'Populate Database with the 12 Moroccan States (Régions) Agora mouad',
+    'summary': 'Populate Database with the 12 Moroccan States (Régions) Agora mouadbanane',
     'version': '8.0.0.1.0',
     'category': 'Morocco Localization',
     'author': "Agora Developpement, Odoo Community Association (OCA)",
